36df378bdf8edd8184411f6f007d45856859f7fb
aaoshepkov <aaoshepkov@gmail.com>
2025-04-17 16:04:55 +0300

    Правки для отправки на проверку

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from src.masks import get_mask_account, get_mask_card_number
-from src.widget import mask_account_card, get_date
-
+from src.widget import get_date, mask_account_card
 
 print(get_date("2024-03-11T02:26:18.671407"))
-print(mask_account_card("Maestro 70007922896063621"))
-print(get_mask_card_number("2022154687962548"))
-print(get_mask_account("14751428002000200035"))
+print(mask_account_card("Maestro 7000792289606362"))
+print(mask_account_card("Счет 73654108430135874305"))
