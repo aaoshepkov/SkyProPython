07044172081bf9720b84654d7ff6aa661029aef6
aaoshepkov <aaoshepkov@gmail.com>
2025-04-30 08:54:59 +0300

    Обновление файла widget.py

--- src/widget.py
+++ src/widget.py
@@ -9,16 +9,16 @@ def mask_account_card(card_or_acc_num: str) -> str:
     :param card_or_acc_num: принимает на вход строку с номером карты, либо номером счета
     :return: возвращает маскированный номер карты или номер счета (маскировка производится модулем masks.py)
     """
-    acc_num = ""
-    for num in card_or_acc_num:
-        if num.isdigit():
-            acc_num += num
-    if len(acc_num) == 16:
-        return get_mask_card_number(acc_num)
-    elif len(acc_num) == 20:
-        return get_mask_account(acc_num)
-    else:
-        return "Номер должен содержать 16 (карта) или 20 (счет) цифр"
+    acc_num = card_or_acc_num.split()
+    for num in acc_num:
+        if num.isdigit() and len(num) == 16:
+            return get_mask_card_number(num)
+    # if len(acc_num) == 16:
+    #     return get_mask_card_number(acc_num)
+    # elif len(acc_num) == 20:
+    #     return get_mask_account(acc_num)
+    # else:
+    #     return "Номер должен содержать 16 (карта) или 20 (счет) цифр"
 
 
 def get_date(date: str) -> str:
@@ -29,3 +29,4 @@ def get_date(date: str) -> str:
     """
     date_formatted = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
     return date_formatted.strftime("%d.%m.%Y")
+print(get_mask_account("Visa Platinum 7000792289606361"))
