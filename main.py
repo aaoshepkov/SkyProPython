89ee0ad7a04b26706284c928b8a65de2a2eb0b34
aaoshepkov <aaoshepkov@gmail.com>
2025-04-30 09:03:40 +0300

    fixes for dev

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ from src.widget import get_date, mask_account_card
 print(get_date("2024-03-11T02:26:18.671407"))
 print(mask_account_card("Maestro 7000792289606362"))
 print(mask_account_card("Счет 73654108430135874305"))
+
+
+#new branch
