416f12ffad2bfe00d0dba736ae9d11e64de3772f
aaoshepkov <aaoshepkov@gmail.com>
2025-05-01 13:35:04 +0300

    Исправления домашки в файле processing.py. Использовал get() вместо item() для получения значения по ключу

--- src/processing.py
+++ src/processing.py
@@ -31,4 +31,3 @@ def sort_by_date(transactions_list: list, sorting_asc: bool = True) -> list:
         return sorted(
             transactions_list, key=lambda x: datetime.strptime(x["date"], "%Y-%m-%dT%H:%M:%S.%f"), reverse=True
         )
-print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
