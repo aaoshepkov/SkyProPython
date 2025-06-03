1db8cb1b9c2d2b0616221930d498d0f989b20215
aaoshepkov <aaoshepkov@gmail.com>
2025-06-03 21:44:57 +0300

    added docstrings in test_masks.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -14,8 +14,18 @@ def correct_account_number():
 
 
 def test_get_mask_card_number(correct_card_number):
+    '''
+    Testing masking a correct card number
+    :param correct_card_number: '4016223056204055'
+    :return: test has to be in PASSED status
+    '''
     assert get_mask_card_number(correct_card_number) == 'Номер карты 4016 22** **** 4055'
 
 
 def test_get_mask_account(correct_account_number):
+    '''
+    Testing masking a correct account number
+    :param correct_card_number: '2501620256234890001'
+    :return: test has to be in PASSED status
+    '''
     assert get_mask_account(correct_account_number) == "Номер счета **0001"
