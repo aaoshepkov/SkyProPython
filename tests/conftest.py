aeb351b390a0f796af22c7db71b46cb538876aaf
aaoshepkov <aaoshepkov@gmail.com>
2025-06-04 21:53:02 +0300

    правки по mypy

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
-from typing import Tuple, List, Dict, Any
+from typing import Any, Dict, List, Tuple
+
 import pytest
 
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,8 @@
-import pytest
-from typing import List, Dict, Any
-from src.processing import filter_by_state
+from typing import Any, Dict, List
 
+import pytest
+
+from src.processing import filter_by_state
 
 data = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
