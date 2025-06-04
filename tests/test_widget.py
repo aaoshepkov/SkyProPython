75841afb7b584bb77c0fd23067669a8dbe0dd8db
aaoshepkov <aaoshepkov@gmail.com>
2025-06-04 08:41:24 +0300

    добавил дату в main.py, исправил тест на дату, также исправил виджет

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,7 +1,16 @@
 import pytest
 from src.widget import mask_account_card
+from src.widget import get_date
 
 
-@pytest.mark.parametrize('num,  expected', [("4016223056204055", "Номер карты 4016 22** **** 4055"), ("25016202562348900001", "Номер счета **0001")])
+@pytest.mark.parametrize('num,  expected', [
+    ("4016223056204055", "Номер карты 4016 22** **** 4055"),
+    ("25016202562348900001", "Номер счета **0001")
+    ])
 def test_widget(num, expected):
     assert mask_account_card(num) == expected
+
+
+@pytest.mark.parametrize("date, expected", [("")])
+def test_get_date(date, expected):
+    assert get_date(date) == expected
