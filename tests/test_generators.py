fe1eba969be0ce536b4df26439ec093ee9fc4084
aaoshepkov <aaoshepkov@gmail.com>
2025-06-08 20:45:36 +0300

    Добавлено описание нового модуля в readme.md, убрано лишнее из самих модулей (тестовые принты и прочее)

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,7 +1,4 @@
-import pytest
-from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
-from tests.conftest import transactions_list
-from tests.test_main import expected
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 
 
 def test_filter_by_currency(transactions_list):
@@ -12,47 +9,29 @@ def test_filter_by_currency(transactions_list):
             "id": 939719570,
             "state": "EXECUTED",
             "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
+            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
             "description": "Перевод организации",
             "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
+            "to": "Счет 11776614605963066702",
         },
         {
             "id": 142264268,
             "state": "EXECUTED",
             "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
+            "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
             "description": "Перевод со счета на счет",
             "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
+            "to": "Счет 75651667383060284188",
         },
         {
             "id": 895315941,
             "state": "EXECUTED",
             "date": "2018-08-19T04:27:37.904916",
-            "operationAmount": {
-                "amount": "56883.54",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
+            "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
             "description": "Перевод с карты на карту",
             "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229"
-        }
+            "to": "Visa Platinum 8990922113665229",
+        },
     ]
 
     assert result == expected
@@ -61,12 +40,15 @@ def test_filter_by_currency(transactions_list):
 def test_transaction_descriptions(transactions_list):
     result = list(transaction_descriptions(transactions_list))
     expected = [
-        'Перевод организации', 'Перевод со счета на счет', 'Перевод со счета на счет',
-        'Перевод с карты на карту', 'Перевод организации'
+        "Перевод организации",
+        "Перевод со счета на счет",
+        "Перевод со счета на счет",
+        "Перевод с карты на карту",
+        "Перевод организации",
     ]
     assert result == expected
 
 
 def test_card_number_generator():
-    result = ["0000 0000 0000 0001","0000 0000 0000 0002", "0000 0000 0000 0003"]
+    result = ["0000 0000 0000 0001", "0000 0000 0000 0002", "0000 0000 0000 0003"]
     assert list(card_number_generator(1, 3)) == result
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,5 +1,4 @@
 from src.masks import get_mask_account, get_mask_card_number
-from tests.conftest import correct_card_number
 
 
 def test_get_mask_card_number(correct_card_number: str) -> None:
