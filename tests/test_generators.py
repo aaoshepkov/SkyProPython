af4b3703311035190492c5fdc1c8fc288232e86f
aaoshepkov <aaoshepkov@gmail.com>
2025-06-09 17:34:30 +0300

    Убрал импорт из конфтеста из модуля test_generators.py

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,6 +1,5 @@
 import pytest
 from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
-from tests.conftest import transactions_list
 
 
 @pytest.mark.parametrize("data, expected", [([
